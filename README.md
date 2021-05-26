04d9cf871393af2129bb2e60732a96e700ddd245
RahulSharma1802162 <csecec.1802162@gmail.com>
2021-05-26 20:56:03 +0530

    Final Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# For Live Project
+
+GOTO : https://alan-ai-by-rk.netlify.app/
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className={'footer-container'}>
+      <h2>
+        Developer :{' '}
+        <a
+          href='https://www.facebook.com/rahul16933/'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          Rahul Sharma
+        </a>{' '}
+      </h2>
+    </div>
+  );
+};
+
+export default Footer;
